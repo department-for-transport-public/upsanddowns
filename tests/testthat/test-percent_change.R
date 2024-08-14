6904881298bbe5f79728eeda2a1f2df61a373247
Harry Allen <hallen1@gcp-rstudio-test1.virago.internal.dtlr.gov.uk>
2024-08-14 11:00:08 +0000

    Writing tests for unchanged limit arg

--- tests/testthat/test-percent_change.R
+++ tests/testthat/test-percent_change.R
@@ -40,3 +40,14 @@ test_that("changing rounding works", {
   expect_equal(percent_change(0.111, accuracy = 0.1), "up 11.1%")
   expect_equal(percent_change(-0.0125, accuracy = 0.01), "down 1.25%")
 })
+
+test_that("changing unchanged limit works", {
+  expect_equal(percent_change(0.07, unchanged_limit = 0.05), "up 7%")
+  expect_equal(percent_change(-0.06, unchanged_limit = 0.05), "down 6%")
+  expect_equal(percent_change(0.1, unchanged_limit = 0.2), "unchanged")
+  expect_equal(percent_change(-0.15, unchanged_limit = 0.2), "unchanged")
+  expect_equal(percent_change(c(0.32, 0.25), unchanged_limit = 0.15), "up 32% and 25% respectively")
+  expect_equal(percent_change(c(-0.17, -0.23), unchanged_limit = 0.1), "down 17% and 23% respectively")
+  expect_equal(percent_change(c(0.12, -0.03), unchanged_limit = 0.1), "up 12% and unchanged respectively")
+  expect_equal(percent_change(c(0.25, -0.34), unchanged_limit = 0.15), "up 25% and down 34% respectively")
+})
--- tests/testthat/test-pp_change.R
+++ tests/testthat/test-pp_change.R
@@ -46,3 +46,16 @@ test_that("changing rounding works", {
   expect_equal(pp_change(0.111, digits = 1), "up 11.1pp")
   expect_equal(pp_change(-0.0125, digits = 2), "down 1.25pp")
 })
+
+test_that("changing unchanged limit works", {
+  expect_equal(pp_change(0.03, unchanged_limit = 0.02), "up 3pp")
+  expect_equal(pp_change(-0.08, unchanged_limit = 0.04), "down 8pp")
+  expect_equal(pp_change(0.02, unchanged_limit = 0.05), "unchanged")
+  expect_equal(pp_change(-0.06, unchanged_limit = 0.1), "unchanged")
+  expect_equal(pp_change(c(0.43, 0.21), unchanged_limit = 0.05), "up 43pp and 21pp respectively")
+  expect_equal(pp_change(c(-0.19, -0.32), unchanged_limit = 0.1), "down 19pp and 32pp respectively")
+  expect_equal(pp_change(c(0.15, -0.05), unchanged_limit = 0.1), "up 15pp and unchanged respectively")
+  expect_equal(pp_change(c(0.42, -0.28), unchanged_limit = 0.1), "up 42pp and down 28pp respectively")
+})
+
+
